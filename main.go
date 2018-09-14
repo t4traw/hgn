595f4d07161e8f64f534d0daaa3e73a15bfe3c2f
Tatsuro Moriyama <t4traw@gmail.com>
2018-09-14 15:07:23 +0900

    簡単にmdを作成するまで完成

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/urfave/cli"
 )
@@ -11,23 +13,35 @@ func main() {
 	app := cli.NewApp()
 
 	app.Name = "hgn"
-	app.Usage = "hugo generate new post cli tool"
+	app.Usage = "Hugo Generate New post cli tool"
 	app.Version = "0.1"
 
 	app.Action = func(context *cli.Context) error {
-		if context.Bool("cat") {
-			fmt.Println(context.Args().Get(0) + "Meow")
-		} else {
-			fmt.Println(context.Args().Get(0))
+		// ディレクトリ生成
+		t := time.Now()
+		p := t.Format("2006/01")
+		dir := "blog/" + p
+		if err := os.MkdirAll(dir, 0777); err != nil {
+			fmt.Println(err)
 		}
-		return nil
-	}
 
-	app.Flags = []cli.Flag{
-		cli.BoolFlag{
-			Name:  "cat, c",
-			Usage: "Echo with cat",
-		},
+		// ファイル生成
+		d := t.Format("02")
+		f := dir + "/" + d + ".md"
+		file, _ := os.OpenFile(f, os.O_CREATE|os.O_WRONLY, 0777)
+		defer file.Close()
+
+		// Front matter追加
+		var fm []string
+		fm = append(fm, "---")
+		fm = append(fm, "title: ")
+		fm = append(fm, "date: "+t.Format("2006-01-02"))
+		fm = append(fm, "category: ")
+		fm = append(fm, "image: ")
+		fm = append(fm, "---")
+		fmt.Fprintln(file, strings.Join(fm, "\n"))
+
+		return nil
 	}
 
 	app.Run(os.Args)
